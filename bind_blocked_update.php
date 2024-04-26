4b2125f8ff08d24463e1dcf56f65c9393f73f4af
Sergii Spivak <zlobar@corbinatelecom.net>
2024-04-26 13:49:43 +0300

    Initial commit 26.04.2024

---
+++ bind_blocked_update.php
@@ -0,0 +1,45 @@
+#!/usr/local/bin/php
+<?php
+
+$domains_unlock=array();
+$files=glob(dirname(__FILE__)."/whitelist/*");
+foreach ($files as $fname) {
+	unset($file);
+	$file=file($fname);
+	foreach ($file as $line) {
+		$l=strtolower(trim($line));
+		$l=idn_to_ascii($l);
+		if ($l!="") {
+			$domains_unlock[]=$l;
+		}
+	}
+}
+
+$domains=array();
+$files=glob(dirname(__FILE__)."/blocklist/*");
+foreach ($files as $fname) {
+	unset($file);
+	$file=file($fname);
+	foreach ($file as $line) {
+		list($l,)=explode("#",trim($line),2);
+		$l=strtolower($l);
+		$l=idn_to_ascii($l);
+		if ($l!="" && !in_array($l,$domains_unlock)) {
+			$domains[]=$l;
+		}
+	}
+}
+
+$domains=array_unique($domains);
+sort($domains);
+
+$blocked=array();
+foreach ($domains as $dom) {
+	$blocked[]="zone \"${dom}\"\t{ type master; file \"/usr/local/etc/namedb/primary/empty.db\"; };";
+}
+
+file_put_contents(dirname(__FILE__)."/named.conf.blocked", implode("\n",$blocked)."\n");
+file_put_contents(dirname(__FILE__)."/domains.txt", implode("\n",$domains)."\n");
+//exec("/usr/local/sbin/rndc reload");
+
+?>
